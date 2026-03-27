37f7dc20b3f50b2e67ede2d56a9443f56daddac4
yeonsoo <leeyeon990127@gmail.com>
2026-03-27 09:45:42 +0900

    문제풀이 폴더 정리

---
+++ jungol/jungol9206.py
@@ -0,0 +1,32 @@
+N = int(input())
+V = int(input())
+#print(N)
+#print(V)
+
+result_n = N - 10
+result_v = V * 2
+total = result_n + result_v
+
+print(result_n, '+', result_v, "=", total) 
+
+#2--------------------------
+n1 = int(input())
+n2 = int(input())
+print(n1, n2)
+
+n1 = n1 - 10
+n2 *= 2
+
+print(n1, '+', n2, '=', n1 + n2)
+
+#3------------------------------
+print(f"{n1} + {n2} = {n1 + n2}")
+
+#4------------------------------
+a=int(input())
+b=int(input())
+
+c=a-10
+d=b*2
+
+print("{} + {} = {}".format(c,d,c+d))
---
+++ jungol/jungol9207.py
@@ -0,0 +1,8 @@
+n, a = input().split()
+
+print(n, a)
+
+result_floor = int(n) % 2
+print(result_floor)
+
+#a, b, c = input().split()
