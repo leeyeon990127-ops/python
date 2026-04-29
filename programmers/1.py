49f42504d779e88d6c821d7a5541102867fa72a8
yeonsoo <leeyeon990127@gmail.com>
2026-04-29 18:34:05 +0900

    프로그래머스 코드수정정

--- programmers/1.py
+++ programmers/1.py
@@ -12,7 +12,40 @@ def solution(answers):
         if i % 
     
     
+#2 ##################################################################
+def solution(answers):
+    # print(answers)
+    # print(len(answers))
+
+    sp1 = [1, 2, 3, 4, 5]
+    sp2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    sp3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+
+    score1 = 0
+    score2 = 0
+    score3 = 0
+
+    for i in range(len(answers)):
+        if answers[i] == sp1[i % len(sp1)]:
+            score1 += 1
+        if answers[i] == sp2[i % len(sp2)]:
+            score2 += 1
+        if answers[i] == sp3[i % len(sp3)]:
+            score3 += 1
+
+    print(score1, score2, score3)
+    maxScore = max(score1, score2, score3)
     answer = []
+    if maxScore == score1:
+        answer.append(1)
+    if maxScore == score2:
+        answer.append(2)
+    if maxScore == score3:
+        answer.append(3)
+
     return answer
+
+res = solution(ans)
+print(res)
 
 
